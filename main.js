bca1b2ee07235ae05d9f7415cd12485a4b498b94
ship-by-enemy <72682126+ship-by-enemy@users.noreply.github.com>
2021-07-03 11:28:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+Webcam.set({
+  width: 350,
+  height: 300,
+  image_format: 'png', 
+  png_quality: 90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/kb523WEB3/Model.json',modelLoaded);
+function modelLoaded(){
+    console.log('modelLoaded');
+
+}
+function check(){
+img=document.getElementById('captured_image');
+classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+if(error){
+    console.error(error);
+}else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+}
+}
